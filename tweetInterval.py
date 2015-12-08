d50e453f971d118336349ff56f95a8ff31a4e0cc
Mognom <alex.mognom@gmail.com>
2015-12-08 01:37:56 +0100

    hotfix

--- tweetInterval.py
+++ tweetInterval.py
@@ -10,13 +10,17 @@ twitter = api.getApi()
 config = ConfigParser.ConfigParser()
 config.reader("frasesbot.conf")
 
+tweetPeriodico("moonage", config.get("moonage","periodo"), config.get("moonage","frases"))
+
+exit 0
+
 for user in config.sections():
 	print (user)
 	newpid = os.fork()
 	
 	if newpid == 0:
 		if config.get(user,"periodo")) == None:
-			tweetPeriodico(user, config.get(user,"periodo")), config.get(user,"frases")))
+			tweetPeriodico(user, config.get(user,"periodo"), config.get(user,"frases"))
 		else:
 			print("TODO")
 	else:
@@ -25,11 +29,12 @@ for user in config.sections():
 
 #Twittea uno de los mensajes cada periodo segundos
 def tweetPeriodico(user, perido, frases):
-		frases = frases.split('\n')
-
-		for frase in frases:
-			twittear("@" + user + " " + line)
-			time.sleep(periodo)
+	print (frases)
+	print ("--------")
+	frases = frases.split('\n')
+	for frase in frases:
+		twittear("@" + user + " " + line)
+		time.sleep(periodo)
 			
 
 #Intenta twitear un mensaje
