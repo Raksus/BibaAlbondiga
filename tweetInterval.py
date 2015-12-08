9fff0ea316dcc4f538df8893eee0395d84449f95
Mognom <alex.mognom@gmail.com>
2015-12-08 01:31:30 +0100

    Primera implementación de tweets en intervalos.

---
+++ tweetInterval.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+import api
+import tweepy
+import ConfigParser
+import os
+
+twitter = api.getApi()
+
+config = ConfigParser.ConfigParser()
+config.reader("frasesbot.conf")
+
+for user in config.sections():
+	print (user)
+	newpid = os.fork()
+	
+	if newpid == 0:
+		if config.get(user,"periodo")) == None:
+			tweetPeriodico(user, config.get(user,"periodo")), config.get(user,"frases")))
+		else:
+			print("TODO")
+	else:
+		continue
+
+
+#Twittea uno de los mensajes cada periodo segundos
+def tweetPeriodico(user, perido, frases):
+		frases = frases.split('\n')
+
+		for frase in frases:
+			twittear("@" + user + " " + line)
+			time.sleep(periodo)
+			
+
+#Intenta twitear un mensaje
+def twittear(mensaje):
+	try:
+		print("twitea")
+		print(mensaje)
+		#api.update_status(status=mensaje)
+	except:
+		print(":C")
