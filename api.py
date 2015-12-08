4cc402f3c182bfec5bb1684bff60e3a7ce0d4be9
Raksus <irda22r@gmail.com>
2015-12-08 01:25:37 +0100

    favs

---
+++ api.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import tweepy, time, sys, ConfigParser
+
+Config = ConfigParser.ConfigParser()
+Config.read("keys.conf")
+
+# Configurar claves
+CONSUMER_KEY = Config.get("keys", "consumer_key")
+CONSUMER_SECRET = Config.get("keys", "consumer_secret")
+ACCESS_KEY = Config.get("keys", "access_key")
+ACCESS_SECRET = Config.get("keys", "access_secret")
+
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+api = tweepy.API(auth)
+
+def getApi():
+	return api
---
+++ fav.py
@@ -0,0 +1,22 @@
+import api, tweepy, ConfigParser
+
+twitter = api.getApi()
+
+config = ConfigParser.ConfigParser()
+config.read("favs.conf")
+
+last_id = config.get("favs", "last_id")
+
+while True:
+	try:
+		to_compare = twitter.user_timeline(id="moonage180") # count=1
+		for tweet in reversed(to_compare):
+    			if(int(tweet.id_str) > last_id):
+				if tweet.text[:3]!="RT ":
+					twitter.create_favorite(id)
+				last_id = int(tweet.id_str)
+				config.set("favs", "last_id", last_id)
+		with open("favs.conf", "wb") as cfg:
+			config.write(cfg)
+	except tweepy.TweepError as e:
+		print e
